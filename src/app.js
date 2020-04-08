37cda2ed2e9f5e4dc598de6b9d3eb4d9cc1309ca
Pedro Frankiewicz <pedrofranki2@gmail.com>
2020-04-08 19:50:32 -0300

    finish routes

--- src/app.js
+++ src/app.js
@@ -11,25 +11,63 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
+  console.log(repositories);
   return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  const {name, url} = request.body;
-  repositories.push({id: uuid(), name, url});
-  return response.json({id: uuid(), name, url});
+  const {name, url, techs} = request.body;
+  repositories.push({id: uuid(), name, url, techs, likes: 0});
+  return response.json({id: uuid(), name, url, techs, likes: 0});
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {name, techs, url} = request.body;
+
+  const index = repositories.findIndex(item=>item.id == id)
+
+  if(index === -1){
+    return response.status(400).send({error: "id nao encontrado"})
+  }
+
+  repositories[index] = {
+    id,name, techs, url
+  }
+  
+  return response.json(repositories[index]);
+  
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const {id}=req.params;
+
+  const index = repositories.findIndex(item=>item.id === id);
+
+  if(index < 0){
+    return res.status(400).send({error: "id invalido"})
+  }
+  
+  repositories.splice(index, 1);
+
+  return res.status(204).send()
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id}=request.params;
+
+  const index = repositories.findIndex(item=>item.id === id);
+  // console.log(index);
+
+  if(index < 0){
+    return response.status(400).send({error: "id invalido"})
+  }
+
+  repositories[index] = {...repositories[index],likes: repositories[index].likes + 1 }
+  // console.log(repositories[index]);
+
+  return response.json(repositories[index])
 });
 
 module.exports = app;
